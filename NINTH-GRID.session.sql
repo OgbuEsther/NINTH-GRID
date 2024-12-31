2ff3b3a0d9c713903ce1f20ee6b8a347d74dead9
OgbuEsther <ogbuuzoma413@gmail.com>
2024-12-31 14:31:52 +0100

    updating the tables with indexes

--- NINTH-GRID.session.sql
+++ NINTH-GRID.session.sql
@@ -1,18 +1,15 @@
 USE ninthgrid;
 
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE IF NOT EXISTS Photos (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(50),
-    email VARCHAR(100),
-    password VARCHAR(150)
+    photoName VARCHAR(50),
+    photoUrl VARCHAR(100),
+    details VARCHAR(150)
 );
 
-INSERT INTO users (name, email, password) VALUES 
-('Alice', 'alice@example.com', '1234567'),
-('Bob', 'bob@example.com', '1234567'),
-('Esther', 'esther@example.com', '1234567'),
-('Tumi', 'tumi@example.com', '1234567');
+INSERT INTO Photos (photoName , photoName , details) VALUES 
 
-SELECT * FROM users;
+
+SELECT * FROM Photos;
 
 SHOW TABLES IN ninthgrid;
